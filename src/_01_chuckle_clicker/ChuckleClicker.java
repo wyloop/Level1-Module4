07797c92bfa5db628b162380d04b68752acf47a2
wyloop <131419214+wyloop@users.noreply.github.com>
2025-08-23 21:16:19 +0000

    chuckle clicker (IP)

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,22 @@
+package _01_chuckle_clicker;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class ChuckleClicker {
+    public static void main(String[] args) {
+        makeButtons();
+    }
+
+    static void makeButtons() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        JButton buttonOne = new JButton("button 1");
+        frame.add(buttonOne);
+        JButton buttonTwo = new JButton("button 2");
+        frame.add(buttonTwo);
+    }
+}
